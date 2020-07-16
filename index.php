69ffc292f5f8eef785829d118552969f84c94ccf
Corrado <corradoct@hotmail.it>
2020-07-16 14:22:06 +0200

    Ho inserito i file base

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Dischi</title>
+  </head>
+  <body>
+
+  </body>
+</html>
