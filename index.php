a0a06044f763a7cc2ef3912828362bf6bfba79ad
Corrado <corradoct@hotmail.it>
2020-07-16 14:31:42 +0200

    Ho aggiornato i file di base

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
     <!-- Stylesheet -->
     <link rel="stylesheet" href="dist/app.css">
     <title>Dischi</title>
