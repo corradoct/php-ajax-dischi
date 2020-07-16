6a5cc2c37b640d1b09b88c38a1feb955630df706
Corrado <corradoct@hotmail.it>
2020-07-16 17:13:12 +0200

    Ho completato il primo step

--- index.php
+++ index.php
@@ -9,22 +9,41 @@
     <title>Dischi</title>
   </head>
   <body>
-    <?php
-    include __DIR__ . '/database.php';
-    ?>
 
-    <div class="">
-      <?php for ($i=0; $i < count($database); $i++) { ?>
-        <?php $thisDisc = $database[$i]; ?>
-      <ul>
-        <li>
-          <img src="<?php echo $thisDisc['poster']; ?>" alt="">
-          <h2><?php echo $thisDisc['title']; ?></h2>
-          <span><?php echo $thisDisc['author']; ?></span>
-          <span><?php echo $thisDisc['year']; ?></span>
-        </li>
-      </ul>
-      <?php } ?>
+    <?php include __DIR__ . '/database.php'; ?>
+
+    <div class="wrapper">
+
+      <header>
+
+        <div class="logo">
+          <h1>Spotify</h1>
+        </div>
+
+      </header>
+
+      <main>
+
+        <div class="wrapperMain">
+
+          <ul class="discList">
+
+          <?php for ($i=0; $i < count($database); $i++) { ?>
+            <?php $thisDisc = $database[$i]; ?>
+            <li>
+              <img src="<?php echo $thisDisc['poster']; ?>" alt="">
+              <h4><?php echo $thisDisc['title']; ?></h4>
+              <span><?php echo $thisDisc['author']; ?></span>
+              <span><?php echo $thisDisc['year']; ?></span>
+            </li>
+          <?php } ?>
+
+          </ul>
+
+        </div>
+
+      </main>
+
     </div>
 
   </body>
